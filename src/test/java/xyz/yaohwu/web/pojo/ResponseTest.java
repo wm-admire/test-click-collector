5259fdfd08cb1578c24383c615ed53043a238cb6
yaohwu <yaohwu@163.com>
2020-01-07 18:21:49 +0800

    fake fail test

---
+++ src/test/java/xyz/yaohwu/web/pojo/ResponseTest.java
@@ -0,0 +1,20 @@
+package xyz.yaohwu.web.pojo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ResponseTest {
+
+    @Test
+    void status404() {
+        Response response = Response.error(404, "not found", "resource not found");
+        Assertions.assertEquals(404, response.getStatus());
+    }
+
+    @Test
+    void status400() {
+        Response response = Response.error(400, "bad request", "bad request");
+        // fake fail
+        Assertions.assertEquals(401, response.getStatus());
+    }
+}
